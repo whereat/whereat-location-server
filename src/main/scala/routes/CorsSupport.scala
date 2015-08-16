1a1bd2c28891e9bcf96c88a5d3e9ef3ac941c93e
aguestuser <aguestuser@riseup.net>
2015-08-16 01:48:39 -0400

    Add CORS support * via CorsSupport Trait * set `wherat.io` as allowed origin * support preflight OPTION request * borrowed from: https://groups.google.com/forum/#!topic/akka-user/5RCZIJt7jHo

---
+++ src/main/scala/routes/CorsSupport.scala
@@ -0,0 +1,42 @@
+package routes
+
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.model.headers._
+import akka.http.scaladsl.server.{Route, Directive0}
+import akka.http.scaladsl.server.Directives._
+import cfg.Config
+
+/**
+ * Author: @aguestuser, borrowed from: https://groups.google.com/forum/#!topic/akka-user/5RCZIJt7jHo
+ * License: GPLv2 (https://www.gnu.org/licenses/gpl-2.0.html)
+ */
+
+trait CorsSupport extends Config {
+
+  lazy val allowed = { HttpOrigin(allowedOrigin) }
+
+  val corsHandler = { r: Route ⇒
+    accessControlHeaders {
+      preflightRequestHandler ~ r
+    }
+  }
+
+  private def accessControlHeaders: Directive0 = {
+    mapResponseHeaders { headers ⇒
+      `Access-Control-Allow-Origin`.`*` +:
+      `Access-Control-Allow-Credentials`(false) +:
+      `Access-Control-Allow-Headers`(
+        "Accept, Authorization", "Content-Type", "Origin", "X-Requested-With") +:
+      headers
+    }
+  }
+
+  private def preflightRequestHandler: Route = options {
+    complete {
+      HttpResponse(200).withHeaders {
+        `Access-Control-Allow-Methods`( OPTIONS, POST, PUT, GET, DELETE )
+      }
+    }
+  }
+}
--- src/main/scala/routes/Routes.scala
+++ src/main/scala/routes/Routes.scala
@@ -4,63 +4,66 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.Materializer
-import db.{LocationDao}
-import model.{WrappedLocation, Location, JsonProtocols}
-import scala.concurrent.{ExecutionContextExecutor}
+import db.LocationDao
+import model.{JsonProtocols, Location, WrappedLocation}
+
+import scala.concurrent.ExecutionContextExecutor
 
 /**
  * Author: @aguestuser
  * License: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.html)
  */
 
-trait Routes extends JsonProtocols {
+trait Routes extends CorsSupport with JsonProtocols {
 
   implicit val system: ActorSystem
   implicit def executor: ExecutionContextExecutor
   implicit val materializer: Materializer
 
   def route[T <: LocationDao](dao: T): Route =
-    path("hello") {
-      get {
-        complete {
-          "hello world!"
-        }
-      }
-    } ~
-      pathPrefix("locations") {
-        path("init") {
-          post {
-            entity(as[Location]) { loc ⇒
-              completeWith(instanceOf[Seq[Location]]) {
-                completer ⇒ dao.init(loc) map completer
-              }
-            }
-          }
-        } ~
-        path("refresh") {
-          post {
-            entity(as[WrappedLocation]) { case WrappedLocation(loc, lastPing) ⇒
-              completeWith(instanceOf[Seq[Location]]) {
-                completer ⇒ dao.refresh(loc, lastPing) map completer
-              }
-            }
-          }
-        } ~
-        path("remove") {
-          post {
-            entity(as[String]) { id ⇒
-              complete {
-                dao.remove(id) map { n ⇒ s"$n record(s) deleted." }
-              }
-            }
-          }
-        } ~
-        path("erase") {
-          post {
-            complete {
-              dao.erase map { n ⇒ s"Database erased. $n record(s) deleted."}
-            }
+    corsHandler {
+      path("hello") {
+        get {
+          complete {
+            "hello world!"
           }
         }
-      }
+      } ~
+        pathPrefix("locations") {
+          path("init") {
+            post {
+              entity(as[Location]) { loc ⇒
+                completeWith(instanceOf[Seq[Location]]) {
+                  completer ⇒ dao.init(loc) map completer
+                }
+              }
+            }
+          } ~
+            path("refresh") {
+              post {
+                entity(as[WrappedLocation]) { case WrappedLocation(loc, lastPing) ⇒
+                  completeWith(instanceOf[Seq[Location]]) {
+                    completer ⇒ dao.refresh(loc, lastPing) map completer
+                  }
+                }
+              }
+            } ~
+            path("remove") {
+              post {
+                entity(as[String]) { id ⇒
+                  complete {
+                    dao.remove(id) map { n ⇒ s"$n record(s) deleted." }
+                  }
+                }
+              }
+            } ~
+            path("erase") {
+              post {
+                complete {
+                  dao.erase map { n ⇒ s"Database erased. $n record(s) deleted."}
+                }
+              }
+            }
+        }
+    }
 }

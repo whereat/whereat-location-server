c53fad81678ded865125e0501eb2146ecef0c505
The Fisher King <pellam@riseup.net>
2015-09-25 19:04:02 -0400

    Decouple PKPSupport and CORSSupport headers
    Realistically, it makes more sense to let these two headers be discrete
    traits instead of mixing them into one Headers trait.

--- src/main/scala/routes/Headers.scala
+++
@@ -1,9 +0,0 @@
-package routes
-
-import akka.http.scaladsl.server.Route
-
-trait Headers extends PublicKeyPinningSupport with CorsSupport {
- def headers(r:Route):Route = {
-   pkpHandler { corsHandler(r) }
- }
-}

9d242471ae424ddc9dd8b3a8cde64807437aeb15
aguestuser <aguestuser@riseup.net>
2015-10-29 00:06:16 -0400

    Add distinction btw/ dev, prod DB
    * Prod DB is a remote Postgres DB provided by Heroku
    * Url for DB is provided by $DATABASE_URL env variable,
      and can be ommited from `application.conf`
      see [here](http://slick.typesafe.com/doc/3.0.2/database.html#using-a-database-url)
    * Add `Environment` ADT w/ `Dev`/`Prod` subclasses to track environment

--- src/main/scala/cfg/Config.scala
+++ src/main/scala/cfg/Config.scala
@@ -1,8 +1,10 @@
 package cfg
 
 import com.typesafe.config.ConfigFactory
+import slick.driver.H2Driver.api.{Database ⇒ H2Database}
+import slick.driver.PostgresDriver.api.{Database ⇒ PGDatabase}
+
 import scala.util.Properties
-import slick.driver.H2Driver.api._
 
 
 /**
@@ -11,16 +13,27 @@ import slick.driver.H2Driver.api._
   */
 
 trait Config {
-  private val config = ConfigFactory.load()
-  private val httpConfig = config.getConfig("http")
 
+  private val config = ConfigFactory.load()
+  val env = Environment.of(sys.env("WHEREAT_ENVIRONMENT"))
+
+  // db configs
+  val db = env match {
+    case _ ⇒ H2Database.forConfig("localDb")
+    //case _ ⇒ PGDatabase.forURL(sys.env("JDBC_DATABASE_URL"))
+  }
+
+  // http configs
+  private val httpConfig = config.getConfig("http")
   val httpInterface = httpConfig.getString("interface")
   val httpPort = Properties.envOrElse("PORT", httpConfig.getString("port")).toInt
-  val db = Database.forConfig("devDb")
   val allowedOrigin = config.getString("cors.allowed-origin")
+
+  // hpkp configs
   val hpkpPinnedKey = sys.env("WHEREAT_PKP_PUBLIC_KEY")
   val hpkpBackupKey = sys.env("WHEREAT_PKP_BACKUP_KEY")
   val hpkpEmergencyKey = sys.env("WHEREAT_PKP_EMERGENCY_KEY")
   val hpkpReportURI = sys.env("WHEREAT_PKP_REPORT_URI")
   val hpkpMaxAge = sys.env("WHEREAT_PKP_MAX_AGE")
  }
+

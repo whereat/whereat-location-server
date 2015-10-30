8af229662a016eff3df516c5eed56a98af4f991a
aguestuser <aguestuser@riseup.net>
2015-10-30 15:03:16 -0400

    Spurious change
    * for purposes of allowing re-push to heroku remote

--- src/main/scala/model/Message.scala
+++ src/main/scala/model/Message.scala
@@ -4,7 +4,6 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.DefaultJsonProtocol
 
 /**
- * Author: @aguestuser
  * License: GPLv2 (https://www.gnu.org/licenses/gpl-2.0.html)
  */
 

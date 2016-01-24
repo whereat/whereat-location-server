aa298fd3b364dbc12e251d7e2f9df78fbb00b278
aguestuser <aguestuser@riseup.net>
2016-01-24 17:54:04 -0500

    Add Copyright/License boilerplate to all files
    Side effects:
    * Revise notice language in LICENSE.md
    * Streamline code of conduct language in CONTRIBUTING.md

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,20 @@
+/**
+ *
+ * Copyright (c) 2015-present, Total Location Test Paragraph.
+ * All rights reserved.
+ *
+ * This file is part of Where@. Where@ is free software:
+ * you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License (GPL), either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Where@ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. For more details,
+ * see the full license at <http://www.gnu.org/licenses/gpl-3.0.en.html>
+ *
+ */
+
 import actors.{EraseActor, Erasable}
 import akka.actor.{Props, ActorSystem}
 import akka.http.scaladsl.Http
@@ -6,13 +23,6 @@ import cfg.Config
 import db.LocationDaoImpl
 import routes.Routes
 import scala.concurrent.duration._
-
-
-
-/**
- * Author: @aguestuser
- * License: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.html)
- */
 
 
 object Main extends App with Config with Routes with Erasable {
